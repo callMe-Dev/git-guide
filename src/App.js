bb0fab0d2a3d31fa77ea6773d209a3148c2eedc2
mrLuisFer <lolesuncrak@gmail.com>
2021-02-10 11:01:19 -0600

    feat: node-sass added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Header from './components/Header/Header';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
