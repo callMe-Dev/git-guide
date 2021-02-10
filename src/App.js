0572ea0ed3a49b5bc30e08b6f3935f2e620aae1e
mrLuisFer <mrluisfer@mrLuisFer.localdomain>
2021-02-09 18:40:23 -0600

    feat: tailwind config added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-blue-700"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import "tailwindcss/tailwind.css"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
+render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
