8da592f3cb7cd5fb7010c9bffd6ea91e2a33811e
mrLuisFer <mrluisfer@mrLuisFer.localdomain>
2021-02-09 22:30:23 -0600

    feat: prettier config added

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link text-blue-700"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link text-blue-700'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
