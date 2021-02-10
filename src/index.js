c8c0b4d5529bfd7eb278cf4a6215dc833b3139ef
mrLuisFer <lolesuncrak@gmail.com>
2021-02-10 10:31:31 -0600

    fix: removing tailwind for use sass

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-import 'tailwindcss/tailwind.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
