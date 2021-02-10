ef5f04003e9d47eb7c9e098597adc0f18fdceaab
mrLuisFer <mrluisfer@mrLuisFer.localdomain>
2021-02-09 23:56:55 -0600

    feat: Add a little structure before creating the layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link text-blue-700'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header className='header layout'>
+      <p className='header-title'>
+        <i className='fab fa-git-alt'></i>Git guide
+      </p>
+    </header>
+  );
+}
