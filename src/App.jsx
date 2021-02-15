fdaaa1019e798c2f231414d391333df3b38994ea
mrLuisFer <lolesuncrak@gmail.com>
2021-02-14 23:29:06 -0600

    fix: I added the functionality as well as the use of IntersectionObserver

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 // Views
 import Layout from './views/Layout/Layout';
-// Components
-import BtnToTop from './components/BtnToTop/BtnToTop';
 
 function App() {
   return (
     // Aqui podemos seleccionar que elementos se van a renderizar
     <div className='App'>
       <Layout />
-      <BtnToTop />
     </div>
   );
 }
--- src/views/Layout/Layout.jsx
+++ src/views/Layout/Layout.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { useInView } from 'react-intersection-observer';
 // Components
 import ContentComponent from '../../components/ContentComponent/ContentComponent';
 import NavMobile from '../../components/NavMobile/NavMobile';
 import MenuMobile from '../../components/MenuMobile/MenuMobile';
 import InstallWindows from '../../components/InstallWindows/InstallWindows';
+import BtnToTop from '../../components/BtnToTop/BtnToTop';
 
 export default function Layout() {
   const [showMenu, setShowMenu] = useState(false);
 
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
+
   return (
     <Router>
       <div className='Layout'>
-        <NavMobile setShowMenu={setShowMenu} showMenu={showMenu} />
+        <NavMobile
+          setShowMenu={setShowMenu}
+          showMenu={showMenu}
+          reference={ref}
+        />
         {showMenu ? (
           <MenuMobile setShowMenu={setShowMenu} showMenu={showMenu} />
         ) : (
@@ -25,6 +35,7 @@ export default function Layout() {
             </Route>
           </Switch>
         )}
+        <BtnToTop inView={inView} />
       </div>
     </Router>
   );
