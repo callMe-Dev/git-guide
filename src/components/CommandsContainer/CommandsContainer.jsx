2e9e0b15eb97f5fff20b30fdf2a513d8eeeccab2
Luis Alvarez <65029792+mrLuisFer@users.noreply.github.com>
2021-02-26 17:34:14 -0600

    feat & fix: agregue y corregi una manera de agregar comandos de manera simple y rapida (#14)

--- src/components/CommandsContainer/CommandsContainer.jsx
+++ src/components/CommandsContainer/CommandsContainer.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import { useHistory, useRouteMatch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
+import CommandName from './CommandName/CommandName';
 
 export default function CommandsContainer() {
-  let history = useHistory();
-  /*
-   * Esto da la URL en la que se encuentra que seria '/commands'
-   *
-   * Y solo agrega el texto de la derecha
-   */
-  const match = useRouteMatch();
-
-  const handlePushView = (viewText) => {
-    if (viewText === undefined) return;
-
-    // Esto enviara al usuario al texto que se coloque dependiendo del comando
-    // En este caso es '/commands/${viewText}'
-    history.push(`${match.url}/${viewText}`);
-  };
-
   return (
-    <div className='CommandContainer'>
-      <p
-        className='CommandContainer__command__name'
-        onClick={() => handlePushView('git-init')}
-      >
-        <i className='fas fa-hashtag'></i>
-        <span>git</span> init
-      </p>
-    </div>
+    <Switch>
+      <Route path='/commands'>
+        <div className='CommandContainer'>
+          <CommandName extCmdName='init' />
+          <CommandName extCmdName='status' />
+        </div>
+      </Route>
+    </Switch>
   );
 }
---
+++ src/components/CommandsContainer/commands/gitInit/gitInit.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function GitInit() {
+  return (
+    <div>
+      <p>git init</p>
+    </div>
+  );
+}
