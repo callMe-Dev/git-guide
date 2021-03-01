73ce2f9de09e0ba6a756e81f989fa59eca04bfdb
LCV13 <lourdesvillarreal109@gmail.com>
2021-03-01 20:21:47 -0300

    Terminé la vista de MacOs

--- src/components/InstallMacOs/InstallMacos.jsx
+++ src/components/InstallMacOs/InstallMacos.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function MacOs() {
   return(
-    <section className="InstallMacOs">
+    <section className="InstallMacos">
       <h1 className="InstallMacos__title">
         Como instalar <span>Git</span> en Mac Os
       </h1>
-      <article className="InstallMacOs__content">
+      <article className="InstallMacos__content">
         <p>
           La forma más sencilla de instalar git en tu Mac es a través de la línea de comandos.
         </p>
@@ -14,14 +14,14 @@ export default function MacOs() {
           Para eso, debes dirigirte a la página web de <a href="https://brew.sh/index_es">Homebrew</a> donde verás un comando similar a este:
         </p>
         <code>
-        /bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+          $curl https://raw.githubusercontent.com/Homebrew...
         </code>
         <p>Tienes que copiarlo y pegarlo en la terminal de Mac (para abrirla, puedes abrir la carpeta Aplicaciones, luego Utilidades y hacer doble clic en Terminal).</p>
         <p>
           Una vez finalizado esto, puedes reiniciar la consola y ejecutar el siguiente comando:
         </p>
         <code>
-          brew install git
+          $ brew install git
         </code>
         <p>
           Listo! Ya puedes usar Git en tus proyectos :D
