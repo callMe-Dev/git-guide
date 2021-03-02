32d36957cb465407853997dd88d0265f713c4e89
mrLuisFer <lolesuncrak@gmail.com>
2021-03-01 23:23:11 -0600

    fix: corregi varios detalles de los estilos en version desk, y con el menu en mobile

--- src/components/AnchorTag/AnchorTag.jsx
+++ src/components/AnchorTag/AnchorTag.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+/*
+ * AnchorTag es solo un componente de etiqueta <a> pero con mas funcionalidades
+ * @anchorText = string
+ * @url = string
+ * @anchorKey? = string
+ */
 export default function AnchorTag({ anchorText, url, anchorKey = '' }) {
   return (
     <a href={url} target='_blank' rel='noreferrer' accessKey={anchorKey}>
--- src/components/InstallMacOs/InstallMacos.jsx
+++ src/components/InstallMacOs/InstallMacos.jsx
@@ -1,30 +1,59 @@
 import React from 'react';
+import AnchorTag from '../AnchorTag/AnchorTag';
 
 export default function MacOs() {
-  return(
-    <section className="InstallMacos">
-      <h1 className="InstallMacos__title">
-        Como instalar <span>Git</span> en Mac Os
+  return (
+    <section className='InstallMacos'>
+      <h1 className='InstallMacos__title'>
+        Como instalar <span>Git</span> en MacOs
       </h1>
-      <article className="InstallMacos__content">
+      <article className='InstallMacos__content'>
         <p>
-          La forma más sencilla de instalar git en tu Mac es a través de la línea de comandos.
+          La forma más sencilla de instalar git en tu <span>Mac</span> es a
+          través de la línea de comandos.
         </p>
         <p>
-          Para eso, debes dirigirte a la página web de <a href="https://brew.sh/index_es">Homebrew</a> donde verás un comando similar a este:
+          Para eso, debes dirigirte a la página web de{' '}
+          {/* La etiqueta <a> da varios errores si se usa sola, por eso usamos el componente
+              AnchorTag
+          */}
+          <AnchorTag
+            url='https://brew.sh/index_es'
+            anchorText='Homebrew'
+            anchorKey='git on macOs'
+          />{' '}
+          donde verás un comando similar a este:
         </p>
         <code>
-          $curl https://raw.githubusercontent.com/Homebrew...
+          $ curl <span>https://raw.githubusercontent.com/Homebrew...</span>
         </code>
-        <p>Tienes que copiarlo y pegarlo en la terminal de Mac (para abrirla, puedes abrir la carpeta Aplicaciones, luego Utilidades y hacer doble clic en Terminal).</p>
         <p>
-          Una vez finalizado esto, puedes reiniciar la consola y ejecutar el siguiente comando:
+          Tienes que copiarlo y pegarlo en la terminal de <span>Mac</span> (para
+          abrirla, puedes abrir la carpeta Aplicaciones, luego Utilidades y
+          hacer doble clic en Terminal).
+        </p>
+        <p>
+          Una vez finalizado esto, puedes reiniciar la consola y ejecutar el
+          siguiente comando:
         </p>
         <code>
-          $ brew install git
+          $ brew <span>install git</span>
         </code>
         <p>
-          Listo! Ya puedes usar Git en tus proyectos :D
+          Si no quieres tener que hacer mas comandos o tienes algun problema con
+          tu shell
+        </p>
+        <p>
+          El equipo de <span>Git</span> mantiene un paquete binario para{' '}
+          <span>MacOs</span> y está disponible para su descarga en el sitio web
+          de Git
+        </p>
+        <AnchorTag
+          url=' https://git-scm.com/download/mac'
+          anchorText='git-scm.com/download/mac'
+        />
+        <p>
+          Listo! Ya puedes usar Git en tus proyectos <span>:D</span>
         </p>
       </article>
     </section>
