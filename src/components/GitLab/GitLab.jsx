e1fccaa451eb04946429a44671d8de078cbcceba
LCV13 <lourdesvillarreal109@gmail.com>
2021-03-05 19:27:12 -0300

    Agregué la vista de GitLab

--- src/components/GitLab/GitLab.jsx
+++ src/components/GitLab/GitLab.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import createProjectGitLab from '../../assets/gitLabAssets/create-project-gitlab.png';
+import formGitLab from '../../assets/gitLabAssets/form-gitlab.png';
+import repositoryGitLab from '../../assets/gitLabAssets/repository-gitlab.png';
 
 export default function GitLab() {
   return (
     <div>
-      <h1>Vista de GitLab</h1>
+      <h1>Crea tu repositorio en GitLab</h1>
+      <p>
+        Una vez que tengas tu cuenta en 
+        <a target='_blank'
+          rel='noreferrer'
+          accessKey='git install windows'href="https://gitlab.com/">
+            GitLab 
+        </a> 
+        debes dirigirte a la vista de Projects elegir la opción <b>Create Blank Project</b>:
+      </p>
+      <img src={createProjectGitLab} alt="Create Project GitLab"/>
+      <p>
+        Luego debes rellenar los campos con la información de tu proyecto y darle click al botón <b>Create project</b>
+      </p>
+      <img src={formGitLab} alt="form GitLab"/>
+      <p>
+        Cuando termines, verás la opción <b>New File</b>, dale click y sube tu proyecto :)
+      </p>
+      <img src={repositoryGitLab} alt="repository GitLab"/>
     </div>
   )
 }
-
 
