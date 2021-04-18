cd0b5bb268f5efd6dd7ee89662b6477938adfe7c
mrLuisFer <lolesuncrak@gmail.com>
2021-04-18 00:15:48 -0500

    feat: agregue contenido a la vista bitbucket

--- src/views/Repositories/Bitbucket/Bitbucket.jsx
+++ src/views/Repositories/Bitbucket/Bitbucket.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import LazyImg from '../../../components/LazyImg/LazyImg'
 import MetaHelmet from '../../../components/MetaHelmet/MetaHelmet'
+
+import createRepoBitbucket from '../../../assets/bitbucket/create-repo.png'
+import createProjectBitbucket from '../../../assets/bitbucket/create-project.png'
 
 export default function Bitbucket() {
   return (
@@ -13,7 +17,37 @@ export default function Bitbucket() {
         Como crear un repositorio en <span>Bitbucket</span>
       </h1>
       <article className='Repository__content'>
-        <p></p>
+        <p>
+          Luego de crear tu cuenta en <span>Bitbucket</span>, solo debes dar el
+          signo más o crear <span>Repositorio</span>
+        </p>
+        <p>
+          Tambien ahi mismo puedes importar mas repositorios, que ya hayas hecho
+          o como si quisieras clonar uno
+        </p>
+        <LazyImg
+          source={createRepoBitbucket}
+          imgAlt='Crear un repositorio en Bitbucket'
+          imgClass='Bitbucket-img'
+        />
+
+        <p>Luego, complete los datos que necesita para su proyecto</p>
+        <p>Además de un nombre, descripción y un avatar de su preferencia</p>
+        <LazyImg
+          source={createProjectBitbucket}
+          imgAlt='Fomulario del repositorio en Bitbucket'
+          imgClass='Bitbucket-img'
+        />
+
+        <p>
+          Despues de eso podras clonar el repositorio en tu{' '}
+          <span>maquina local</span> y poder agregar nuevas features o iniciar
+          el proyecto de tus sueños
+        </p>
+        <code>
+          $ git <span>clone</span> <br /> https://usuario@bitbucket.org/user/
+          repositorio/carpeta_destino
+        </code>
       </article>
     </section>
   )
