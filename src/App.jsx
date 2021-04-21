91609d0791e8a1cfd8dabab08d785e820473e399
Luis <mluise2406@gmail.com>
2021-04-20 18:05:30 -0600

    agregando useState para probar el darkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from "react"
 // Views
 import Layout from './views/Layout/Layout'
 
 function App() {
+
+  const theme = localStorage.getItem('theme')
+  
+  const [dark, setDark] = useState('')
+   
+  useEffect(() => {
+    if(!theme) localStorage.setItem('theme', 'light')
+    if(theme === 'dark'){
+      setDark('Dark')
+    }else{
+      setDark('')
+    }
+  },[theme])
+
   return (
-    <div className='App'>
+    <div className={`App ${dark}`}>
       <div className='App__grid'>
         <Layout />
       </div>
