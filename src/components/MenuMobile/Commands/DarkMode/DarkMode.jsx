0b6bcd3b94dc54ef82ba4b200e55fe820ef07c15
Luis <mluise2406@gmail.com>
2021-04-24 13:58:28 -0600

    style: icono para DarkMode agregado

--- src/components/MenuMobile/Commands/DarkMode/DarkMode.jsx
+++ src/components/MenuMobile/Commands/DarkMode/DarkMode.jsx
@@ -1,48 +1,68 @@
 import React, { useState, useRef, useEffect } from 'react'
+import PropTypes from "prop-types"
 
 export default function DarkMode({ setImgTheme }) {
   const toggleRef = useRef()
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light')
+  const [icon, setIcon] = useState('fa-sun')
   const body = document.body
 
   useEffect(() => {
-    if(!theme){
-      localStorage.setItem('theme', 'light')
-    }
-    theme === 'dark' ? body.classList.add('Dark') : 'light'
-
+    checkTheme()
   }, [])
 
-  const changeTheme = () => {
-    if(toggleRef.current.checked){
-      setTheme('Dark') 
-      setImgTheme('dark')
-      localStorage.setItem('theme', 'dark')
-      body.classList.remove('light')
+  const checkTheme = () => {
+    if (!theme) {
+      localStorage.setItem('theme', 'light')
+    }
+    if (theme === 'dark') {
       body.classList.add('Dark')
-    }else{
+      setIcon('fa-moon')
+    } else {
+      body.classList.add('Light')
+      setIcon('fa-sun')
+    }
+  }
+
+  const changeTheme = () => {
+    if (toggleRef.current.checked) {
+      setTheme('Dark')
+      setImgTheme('dark')
+      setIcon('fa-moon')
+      localStorage.setItem('theme', 'dark')
+      body.classList.replace('Light', 'Dark')
+    } else {
       setTheme('light')
       setImgTheme('light')
+      setIcon('fa-sun')
       localStorage.setItem('theme', 'light')
-      body.classList.remove('Dark')
-      body.classList.add('light')
+      body.classList.replace('Dark', 'Light')
     }
   }
 
   return (
     <div className='DarkMode'>
-      <h1 className='DarkMode__Title'>Dark Mode</h1>
-      <label className='DarkMode__Switch'>
-        <input
-          ref={toggleRef}
-          type='checkbox'
-          onChange={changeTheme}
-          checked={theme === 'light' ? false : true}
-          name=''
-          id=''
-        />
-        <span className='Slider Round'></span>
-      </label>
+      <span></span>
+      <div>
+        <span className='text-2xl self-center'>
+          <i className={`fas DarkMode__Icon ${icon}`}></i>
+        </span>
+        <label className='DarkMode__Switch'>
+          <input
+            ref={toggleRef}
+            type='checkbox'
+            onChange={changeTheme}
+            checked={theme === 'light' ? false : true}
+            name=''
+            id=''
+          />
+          <span className='Slider Round'></span>
+        </label>
+      </div>
     </div>
   )
+}
+
+DarkMode.propTypes = {
+  setImgTheme: PropTypes.func.isRequired  
 }
