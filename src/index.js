46802f9b2e2fd6256cbc7549e84fd1d04dfaef45
mrLuisFer <mrluisfeer@gmail.com>
2022-03-26 13:25:53 -0600

    change to dart sass instead of node-sss

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { render } from 'react-dom'
 import App from './App'
 import './index.scss'
+import 'sass'
 
 const root = document.getElementById('root')
 
